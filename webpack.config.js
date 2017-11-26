87f6babbb936689cb97f34fa69c0bc815e7c226f
xiexuanzheng123 <349328984@qq.com>
2017-11-26 22:06:22 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        index: [
+            'babel-polyfill',
+            './js/pie/pie'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader']
+            },{
+                test: /\.css/,
+                use: ['style-loader', 'css-loader']
+            },{
+                test: /\.(jpe?g|png|gif)$/,
+                use: ['url-loader?limit=10000&name=img/[hash:8].[name].[ext]']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.css']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Echarts',
+            filename: 'build/echarts.html',
+            template: './index.html',
+            inject: 'body',
+            hash: true,
+            chunks: ['index']
+        })
+    ]
+}
