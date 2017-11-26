68fca016d1fe25dce7aec544da0bb644ae0b687a
xiexuanzheng123 <349328984@qq.com>
2017-11-26 22:06:54 +0800

    bar

---
+++ js/bar/bar.js
@@ -0,0 +1,18 @@
+import * as echarts from 'echarts';
+
+let myChart = echarts.init(document.getElementById('main'));
+myChart.setOption({
+    title: {
+        text: 'Echarts入门实例'
+    },
+    tooltip: {},
+    xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+});
