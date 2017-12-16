bf68022d9db0a74310301c8c5945498b0ae92dff
xiexuanzheng123 <349328984@qq.com>
2017-12-16 21:03:04 +0800

    map of china

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: {
         index: [
             'babel-polyfill',
-            './js/bar/bar'
+            './js/visualMapPiecewise/scatter'
         ]
     },
     output: {
